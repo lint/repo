65579e9c33dad7130c5ca4a0f2f4856f17edca23
System Administrator <root@iphone.fios-router.home>
2019-08-08 13:05:05 -0400

    Updating Files

--- update.sh
+++ update.sh
@@ -2,8 +2,10 @@
 
 ./dkpg-scanpackages debs /dev/null > Packages
 
-
-rm Packages.gz
-rm Packages.bz2
+rm Packages.gz Packages.bz2
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
+
+git add --all
+git commit -m "Updating Files"
+git push origin master
