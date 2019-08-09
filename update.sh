68c8fd5e85cabe01b27612d9182e350229548c3b
System Administrator <root@iphone.fios-router.home>
2019-08-08 22:01:33 -0400

    Updating Files

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ rm Packages.gz Packages.bz2
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 xz -c9e Packages > Packages.xz
-xz -c9e > Packages.lzma
+xz -c9e  --format=Packages > Packages.lzma
 
 git add --all
 git commit -m "Updating Files"
