f39a45cc9a5f1998c7a7d1742ea4fdf14a47fda0
System Administrator <root@iphone.fios-router.home>
2019-08-08 21:53:40 -0400

    Updating Files

--- update(less).sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm Packages.gz
-rm Packages.bz2
-gzip -c9 Packages > Packages.gz
-bzip2 -c9 Packages > Packages.bz2
--- update.sh
+++ update.sh
@@ -3,8 +3,10 @@
 ./dpkg-scanpackages debs /dev/null > Packages
 
 rm Packages.gz Packages.bz2
+
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
+xz -c9 Packages > Packages.xz
 
 git add --all
 git commit -m "Updating Files"
